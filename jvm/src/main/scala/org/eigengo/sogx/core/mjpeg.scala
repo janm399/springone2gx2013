9c96e955f9747f6b315d40ef1775784a39393f30
Jan Machacek <jan.machacek@gmail.com>
2013-08-15 15:58:27 +0100

    Updates and Springifying

--- jvm/src/main/scala/org/eigengo/sogx/core/mjpeg.scala
+++ jvm/src/main/scala/org/eigengo/sogx/core/mjpeg.scala
@@ -1,12 +1,32 @@
 package org.eigengo.sogx.core
 
-import java.util.UUID
 import java.util
+import org.eigengo.sogx._
+import scala.collection.mutable.ArrayBuffer
+import java.util.Collections
 
 class MJPEGDecoder {
 
-  def decodeFrames(session: UUID, chunk: Array[Byte]): util.Collection[Array[Byte]] = {
-    ???
+  def decodeFrames(correlationId: CorrelationId, chunk: ChunkData): util.Collection[ImageData] = {
+    Collections.singletonList(chunk)
   }
 
+}
+
+case class ChunkingDecoderContext[U](f: Array[Byte] => U) {
+  val buffer: ArrayBuffer[Byte] = ArrayBuffer()
+
+  def decode(chunk: Array[Byte], end: Boolean) {
+    if (buffer.isEmpty && end) {
+      f(chunk)
+    } else {
+      buffer ++= chunk
+      if (end) {
+        f(buffer.toArray)
+        buffer clear()
+      }
+    }
+  }
+
+  def close(): Unit = {}
 }
