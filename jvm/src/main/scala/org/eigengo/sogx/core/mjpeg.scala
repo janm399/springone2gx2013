58b0107bb163643bb7414df6b2453324beff1a39
Jan Machacek <jan.machacek@gmail.com>
2013-08-27 17:43:28 +0100

    Removed in

--- jvm/src/main/scala/org/eigengo/sogx/core/mjpeg.scala
+++ jvm/src/main/scala/org/eigengo/sogx/core/mjpeg.scala
@@ -7,7 +7,7 @@ import java.util.Collections
 class MJPEGDecoder {
 
   def decodeFrames(correlationId: CorrelationId, chunk: Chunk): util.Collection[ImageData] = {
-    Utils.writer.write(s"/Users/janmachacek/x$correlationId.mjpeg", chunk)
+    // Utils.writer.write(s"/Users/janmachacek/x$correlationId.mjpeg", chunk)
     Collections.singletonList(chunk.data)
   }
 
